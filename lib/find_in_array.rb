a10dc16d46a3caa2fb93ad308ef2ae4036398d2f
tiya adugna <tiyakefalee@gmail.com>
2020-04-04 02:10:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   i = 0 
   while i < array.length do 
     if array[i] == value_to_find
-      return array.index(value_to_find) 
+      return i 
   end
   i += 1 
   
